bbae9db0e57eb1b3a7265457801dcf32b32b462c
Martin Disch <martindisch@gmail.com>
2018-06-20 14:16:41 +0200

    Fix loading of large embedded album art

--- app/src/main/java/com/kabouzeid/gramophone/glide/audiocover/AudioFileCoverFetcher.java
+++ app/src/main/java/com/kabouzeid/gramophone/glide/audiocover/AudioFileCoverFetcher.java
@@ -2,6 +2,13 @@ package com.kabouzeid.gramophone.glide.audiocover;
 
 import android.media.MediaMetadataRetriever;
 
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.data.DataFetcher;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -44,7 +51,34 @@ public class AudioFileCoverFetcher implements DataFetcher<InputStream> {
             retriever.release();
         }
 
-        return stream;
+    private static final String[] FALLBACKS = {"cover.jpg", "album.jpg", "folder.jpg"};
+
+    private InputStream fallback(String path) throws FileNotFoundException {
+        // Method 1: use embedded high resolution album art if there is any
+        try {
+            Mp3File mp3File = new Mp3File(path);
+            if (mp3File.hasId3v2Tag()) {
+                ID3v2 id3v2Tag = mp3File.getId3v2Tag();
+                byte[] imageData = id3v2Tag.getAlbumImage();
+                if (imageData != null) {
+                    return new ByteArrayInputStream(imageData);
+                }
+            }
+        // If there are any exceptions, we ignore them and continue to the other fallback method
+        } catch (IOException ignored) {
+        } catch (InvalidDataException ignored) {
+        } catch (UnsupportedTagException ignored) {
+        }
+
+        // Method 2: look for album art in external files
+        File parent = new File(path).getParentFile();
+        for (String fallback : FALLBACKS) {
+            File cover = new File(parent, fallback);
+            if (cover.exists()) {
+                return stream = new FileInputStream(cover);
+            }
+        }
+        return null;
     }
 
     @Override
